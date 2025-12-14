874d4175e133ba1e4fd81333b9873779dcfd6ee3
Jowrenrocks <jowrenrocks123@gmail.com>
2025-12-14 10:30:18 +0800

    Update

--- event-management/database/seeders/AdminSeeder.php
+++ event-management/database/seeders/AdminSeeder.php
@@ -20,17 +20,17 @@ class AdminSeeder extends Seeder
             ['email' => 'jowrenrocks@gmail.com'],
             [
                 'full_name' => 'System Administrator',
-                'password' => Hash::make('password123'),
+                'password' => Hash::make('ambotnibuday'),
                 'role_id' => $adminRole->id,
             ]
         );
 
         // Create default user for testing event creation
         User::updateOrCreate(
-            ['email' => 'user@example.com'],
+            ['email' => 'user@gmail.com'],
             [
                 'full_name' => 'Test User',
-                'password' => Hash::make('password123'),
+                'password' => Hash::make('johnwrendell'),
                 'role_id' => $userRole->id,
             ]
         );  
